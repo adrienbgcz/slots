3c0a9218321cde3c2b1191f33f580587fed98510
adrienbgcz <76441437+adrienbgcz@users.noreply.github.com>
2021-12-22 10:44:14 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Card>
+      <h2>Subtitle</h2>
+      <p>texte</p>
+    </Card>
+    <Card>
+      <button>oui</button>
+    </Card>
+  </div>
+</template>
+
+<script>
+import Card from "./components/Card.vue"
+
+export default {
+  name: 'App',
+  components: {
+    Card
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="card">
+    <slot></slot>
+</div>
+  
+</template>
+
+<script>
+export default {
+    name: "Card"
+}
+
+
+</script>
+
+<style>
+.card {
+    border: solid;
+    color: black
+}
+
+</style>
